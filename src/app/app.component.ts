c4dbd91873b7d97c3a4c11766a09e28f4366b115
OderoOluoch <oderoluoch@gmail.com>
2021-05-18 09:34:48 +0300

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toDoTwo';
+  title = 'My To List';
+
+  todos = [
+    {
+      label:'Bring Milk',
+      done:false,
+      priority:3
+    },
+    {
+      label:'Bring Bread',
+      done:true,
+      priority:2
+    },
+    {
+      label:'Bring Coffee',
+      done:false,
+      priority:1
+    },
+    {
+      label:'Bring Tea Leaves',
+      done:true,
+      priority:3
+    },
+    {
+      label:'Bring Uji',
+      done:false,
+      priority:4
+    },
+    {
+      label:'Bring Sugar',
+      done:true,
+      priority:3
+    },
+
+];
+
+  addTodo(newTodoLabel){
+    var newTodo = {
+      label: newTodoLabel,
+      priority:1,
+      done:false
+    };
+    this.todos.push(newTodo)
+  }
+
+  deletingTodo(todo){
+    this.todos = this.todos.filter(t => t.label !== todo.label );
+  }
 }
